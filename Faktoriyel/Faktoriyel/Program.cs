3acd9ce9ecc9474784774cb44b9b9b9448fd9ed3
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-07 15:43:15 +0300

    first commit

---
+++ Faktoriyel/Faktoriyel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+
+            Console.WriteLine("Faktöriyelini hesaplamak istedigin sayıyı gir.");
+            string input = Console.ReadLine();
+            int x = Convert.ToInt32(input);
+            
+            for(int j = 1; j <= x; j++)
+            {
+                i = i * j;
+            }
+            Console.WriteLine(x+" Sayısının faktöriyeli: "+i+" dir.");
+ 
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci Dizisi Kaçıncı Seriye Kadar Olsun.");
+            string seri = Console.ReadLine();
+            int seri_sayisi = Convert.ToInt32(seri);
+
+            Console.WriteLine("\n" + seri_sayisi +" Serilik Fibonacci Dizisi:\n");
+            int[] fibonacci = new int[seri_sayisi] ;
+
+            fibonacci[0] = 0 ;
+            fibonacci[1] = 1 ;
+            
+
+            for (int i = 0; i <= seri_sayisi; i++)            
+            {
+                fibonacci[i+2] = fibonacci[i] + fibonacci[i+1];
+                Console.WriteLine(fibonacci[i]);
+             
+            }           
+
+        }
+    }
+}
