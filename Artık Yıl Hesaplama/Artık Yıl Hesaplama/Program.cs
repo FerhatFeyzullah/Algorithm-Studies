b134ab31021dc4c018beed12a12381c666bcf7ef
Ferhat <ferhatfeyzullah@hotmail.com>
2025-04-07 19:25:51 +0300

    leap year & mode of number applications

---
+++ Artık Yıl Hesaplama/Artık Yıl Hesaplama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artık_Yıl_Hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lütfen Hesabını Yapmak istediğiniz Yılı Giriniz.\n");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("Girmiş Olduğunuz " + year + " Yılı Artık Yıldır.");
+            }
+            else { Console.WriteLine("Girmiş Olduğunuz " + year + " Yılı Artık Yıl Değildir."); }
+
+
+
+
+        }
+    }
+}
---
+++ Mod Alma/Mod Alma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_Alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bölmek İstediğiniz Sayıyı Giriniz :");
+            int a =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+ " Sayısını bölmek İstediğiniz  Sayıyı Giriniz :" + "\n");
+            int b =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a+" Sayısının "+b +" Sayısına Bölümü :"+ a/b + "\n");
+            Console.WriteLine(a + " Sayısının " + b + " Sayısına Bölümünden Kalan :" + a % b + "\n");
+
+            if (a % 2 == 1)
+            {
+                Console.WriteLine(a + " Sayısı Bir Tek Sayıdır ");
+            }
+            else { Console.WriteLine(a + " Sayısı Bir Çift Sayıdır "); }
+
+
+
+        }
+    }
+}
